61fb8886caa0aed4f307a191fb4ec68a6dea6129
Yubraj-Karki <yubrajkarki528@gmail.com>
2023-05-19 21:23:27 +0545

    db connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const multer = require("multer");
 const upload = multer({ dest: "./uploads" });
+
+// Enable CORS
+app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello from the swiftDrop!");
@@ -15,6 +25,10 @@ app.post("/upload", upload.single("file"), (req, res) => {
   res.send("File uploaded");
 });
 const port = 3000;
+
+const connectDB = require("./dbConnection");
+
+connectDB();
 
 app.listen(port, () => {
   console.log(`Server is listening at port ${port}`);
