98a8d28a6002ca23a989583b7296f2e0f9269031
Yubraj-Karki <68190802+Yubraj-Karki@users.noreply.github.com>
2023-05-20 22:45:10 +0545

    Update dbConnection.js

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
-  "mongodb+srv://me:FWJBqMk.yFVQ2rf@swiftdrop.0cpystm.mongodb.net/?retryWrites=true&w=majority";
+ 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
