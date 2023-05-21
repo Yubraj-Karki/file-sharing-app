74692e010be2b10e108b7fd020e28ed343e40081
YubrajKarki <yubrajkarki528@gmail.com>
2023-05-21 12:14:09 +0545

    db connection

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,29 +1,34 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
- 
+require("dotenv").config();
+const uri = process.env.DATABASE_URL;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
 
-async function connectDB() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+// async function connectDB() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log(
+//       "Pinged your deployment. You successfully connected to MongoDB!"
+//     );
+
+//     // Perform database operations or start your application logic here
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//   }
+// }
+
+function connectDB() {
+  mongoose.connect(process.env.DATABASE_URL);
 }
 
 module.exports = connectDB;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const multer = require("multer");
+const mongoose = require("mongoose");
+const File = require("./models/File");
 const upload = multer({ dest: "./uploads" });
 
 // Enable CORS
@@ -17,14 +19,24 @@ app.get("/", (req, res) => {
   res.send("Hello from the swiftDrop!");
 });
 
-app.post("/upload", upload.single("file"), (req, res) => {
+app.post("/upload", upload.single("file"), async (req, res) => {
   if (!req.file) {
     return res.status(400).send("No file was uploaded.");
   }
-  console.log(req.file);
-  res.send("File uploaded");
+
+  // console.log(req.file);
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname,
+  };
+
+  const file = await File.create(fileData);
+
+  console.log(file);
+
+  res.send(file.originalName);
 });
-const port = 3000;
+const port = 3001;
 
 const connectDB = require("./dbConnection");
 
